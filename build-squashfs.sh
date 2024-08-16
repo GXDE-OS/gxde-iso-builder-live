8f843049729e12e3d867e13ff515b45286899759
gfdgd_xi <3025613752@qq.com>
2024-08-16 10:02:09 +0800

    新增rootf构建脚本

--- build-squashfs.sh
+++ build-squashfs.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
-programPath=$(dirname $0; pwd)
+function UNMount() {
+    sudo umount "$1/sys/firmware/efi/efivars"
+    sudo umount "$1/sys"
+    sudo umount "$1/dev/pts"
+    sudo umount "$1/dev/shm"
+    sudo umount "$1/dev"
+
+    sudo umount "$1/sys/firmware/efi/efivars"
+    sudo umount "$1/sys"
+    sudo umount "$1/dev/pts"
+    sudo umount "$1/dev/shm"
+    sudo umount "$1/dev"
+
+    sudo umount "$1/run"
+    sudo umount "$1/media"
+    sudo umount "$1/proc"
+    sudo umount "$1/tmp"
+}
+programPath=$(cd $(dirname $0); pwd)
 debianRootfsPath=debian-rootfs
+if [[ $1 == "" ]]; then
+    echo 请指定架构：amd64 arm64 loong64
+    exit 1
+fi
+if [[ -d $debianRootfsPath ]]; then
+    UNMount $debianRootfsPath
+    sudo rm -rf $debianRootfsPath
+fi
 sudo apt install debootstrap debian-archive-keyring \
     debian-ports-archive-keyring qemu-user-static -y
 # 构建核心系统
+set -e
 if [[ $1 == loong64 ]]; then
     sudo debootstrap --arch $1 unstable $debianRootfsPath https://mirror.sjtu.edu.cn/debian-ports/
 else
@@ -11,25 +38,37 @@ else
 fi
 # 写入源
 if [[ $1 == loong64 ]]; then
-    sudo cp $programPath/debian-port.list $debianRootfsPath/etc/apt/sources.list.d/debian.list -v
-else
     sudo cp $programPath/debian-unreleased.list $debianRootfsPath/etc/apt/sources.list.d/debian-unreleased.list -v
+else
+    sudo cp $programPath/debian.list $debianRootfsPath/etc/apt/sources.list.d/debian.list -v
+    sudo cp $programPath/99bookworm-backports $debianRootfsPath/etc/apt/preferences.d/ -v
 fi
 sudo sed -i "s/main/main contrib non-free non-free-firmware/g" $debianRootfsPath/etc/apt/sources.list
 sudo cp $programPath/gxde-temp.list $debianRootfsPath/etc/apt/sources.list.d/temp.list -v
+set +e
 # 安装应用
 sudo $programPath/pardus-chroot $debianRootfsPath
 sudo chroot $debianRootfsPath apt update
 sudo chroot $debianRootfsPath apt install gxde-desktop deepin-installer --install-recommends -y
-sudo chroot $debianRootfsPath sudo apt install live-task-recommended live-task-standard live-config-systemd \
+sudo rm -rf $debianRootfsPath/var/lib/dpkg/info/plymouth-theme-gxde-logo.postinst
+sudo chroot $debianRootfsPath apt install live-task-recommended live-task-standard live-config-systemd \
     live-boot -y
-sudo chroot $debianRootfsPath sudo apt install spark-store spark-deepin-wine-runner -y
-sudo chroot $debianRootfsPath sudo apt autopurge wine -y
-sudo chroot $debianRootfsPath sudo aptss update
-sudo chroot $debianRootfsPath sudo aptss full-upgrade -y
+sudo chroot $debianRootfsPath apt install fcitx5-pinyin libudisks2-qt5-0 fcitx5 -y
+sudo chroot $debianRootfsPath apt install spark-store -y
+sudo chroot $debianRootfsPath aptss update
+sudo chroot $debianRootfsPath aptss install spark-deepin-wine-runner -y
+sudo chroot $debianRootfsPath aptss full-upgrade -y
+if [[ $1 == loong64 ]]; then
+    sudo chroot $debianRootfsPath aptss install cn.loongnix.lbrowser -y
+else
+    sudo chroot $debianRootfsPath apt install firefox-esr -y
+fi
+if [[ $1 == arm64 ]] || [[ $1 == loong64 ]]; then
+    sudo chroot $debianRootfsPath aptss install spark-box64 -y
+fi
 # 卸载无用应用
-sudo chroot $debianRootfsPath sudo apt install deepin-terminal -y
-sudo chroot $debianRootfsPath sudo apt purge mlterm deepin-terminal-gtk -y
+sudo chroot $debianRootfsPath apt install deepin-terminal grub-efi network-manager-gnome -y
+sudo chroot $debianRootfsPath apt purge mlterm mlterm-tiny deepin-terminal-gtk ibus -y
 # 安装内核
 if [[ $1 == loong64 ]]; then
     # loong64 安装定制的内核
@@ -45,23 +84,13 @@ sudo chroot $debianRootfsPath apt clean
 sudo rm -rf $debianRootfsPath/var/log/*
 sudo rm -rf $debianRootfsPath/root/.bash_history
 sudo rm -rf $debianRootfsPath/etc/apt/sources.list.d/temp.list
+sudo rm -rf $debianRootfsPath/initrd.img.old
+sudo rm -rf $debianRootfsPath/vmlinuz.old
 # 卸载文件
-sudo umount "$debianRootfsPath/sys/firmware/efi/efivars"
-sudo umount "$debianRootfsPath/sys"
-sudo umount "$debianRootfsPath/dev/pts"
-sudo umount "$debianRootfsPath/dev/shm"
-sudo umount "$debianRootfsPath/dev"
-
-sudo umount "$debianRootfsPath/sys/firmware/efi/efivars"
-sudo umount "$debianRootfsPath/sys"
-sudo umount "$debianRootfsPath/dev/pts"
-sudo umount "$debianRootfsPath/dev/shm"
-sudo umount "$debianRootfsPath/dev"
-
-sudo umount "$debianRootfsPath/run"
-sudo umount "$debianRootfsPath/media"
-sudo umount "$debianRootfsPath/proc"
-sudo umount "$debianRootfsPath/tmp"
+sleep 5
+UNMount $debianRootfsPath
 # 封装
 cd $debianRootfsPath
+set -e
 sudo mksquashfs * ../filesystem.squashfs
+du -h ../filesystem.squashfs
