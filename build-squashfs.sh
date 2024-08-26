caa038ebc6c7338d26e5f0bdfd85f0a9f20bfbcd
gfdgd_xi <3025613752@qq.com>
2024-08-26 00:01:24 -0400

    调整脚本

--- build-squashfs.sh
+++ build-squashfs.sh
@@ -86,6 +86,7 @@ sudo chroot $debianRootfsPath apt install firmware-linux -y
 # 清空临时文件
 sudo chroot $debianRootfsPath apt autopurge -y
 sudo chroot $debianRootfsPath apt clean
+sudo touch $debianRootfsPath/etc/deepin/calamares
 sudo rm -rf $debianRootfsPath/var/log/*
 sudo rm -rf $debianRootfsPath/root/.bash_history
 sudo rm -rf $debianRootfsPath/etc/apt/sources.list.d/temp.list
